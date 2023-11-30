36f4bfedd9ffe3aecb8830c79b301ea517ea670d
Kajal Negi <kajal.negi@algoworks.com>
2023-11-30 14:21:07 +0530

    abc

--- hello.js
+++ hello.js
@@ -1,11 +1,57 @@
-const app = require("express")();
+const express = require('express');
+const multer = require('multer');
+const unzipper = require('unzipper');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const port = 3038;
+const storage = multer.memoryStorage();
+const { exec } = require('node:child_process')
 
-const PORT = process.env.PORT || 3000;
+process.chdir('./DemoProject');
+console.log('directory change to ',process.cwd());
 
-app.get("", (req, res) => {
-  res.send("Hello world");
+exec('npm install eslint-plugin-lwc', (err, output) => {
+    // once the command has completed, the callback function is called
+    if (err) {
+        // log and return if we encounter an error
+        console.error("could not execute command: ", err)
+        return
+    }
+    console.log('directory 0 ',process.cwd());
+    console.log("executed npm install \n", output)
+})
+
+exec('npm install eslint @salesforce/eslint-config-lwc --save-dev', (err, output) => {
+    // once the command has completed, the callback function is called
+    if (err) {
+        // log and return if we encounter an error
+        console.error("could not execute command: ", err)
+        return
+    }
+    console.log('directory 1 ',process.cwd());
+    console.log("executed install eslint \n", output)
+    if(output){
+        fun2();
+    }
+})
+function fun2(){
+exec('npm run lint:lwc', (err, output) => {
+    console.log('directory 2 ',process.cwd());
+    // once the command has completed, the callback function is called
+    if (err) {
+        // log and return if we encounter an error
+        console.error("could not execute lint  command: ", err)
+       // return
+    }
+    // log the output received from the command
+    console.log('directory 3 ',process.cwd());
+    console.log("Output: lint command executed \n", output)
+})
+}
+app.listen(port, () => {
+console.log(`Server is running at http://localhost:${port}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`App up at port ${PORT}`);
-});
+
+//
